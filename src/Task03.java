4d7b019bbc0854c7d7e28ea50cb81a22e4211fab
William-KeLi <2491282230@qq.com>
2022-04-23 14:46:56 +0800

    11

--- src/Task03.java
+++ src/Task03.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Task03 {
     //3．读取7个数（1—50）的整数值，每读取一个值，程序打印出该值个数的*。
     public static void main(String[] args) {
+        printStar();
+    }
+    private static void printStar(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("请依次输入7个数，每次输入完就按一下回车");
         double[] arr = new double[7];
@@ -11,7 +14,7 @@ public class Task03 {
             if (arr[i] < 1 || arr[i] > 50) {
                 System.out.println("输入错误，请重新输入");
                 i--;
-            } else {
+            } else {//
                 for (int j = 0; j <(int) arr[i]; j++) {
                     System.out.print("*");
                 }
