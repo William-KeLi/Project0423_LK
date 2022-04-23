ae1004a8a62c50e87b2c488b94b1ba61c8cf0ea3
William-KeLi <2491282230@qq.com>
2022-04-23 14:41:07 +0800

    11

--- src/Task03.java
+++ src/Task03.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class Task03 {
+    //3．读取7个数（1—50）的整数值，每读取一个值，程序打印出该值个数的*。
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请依次输入7个数，每次输入完就按一下回车");
+        double[] arr = new double[7];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextDouble();
+            if (arr[i] < 1 || arr[i] > 50) {
+                System.out.println("输入错误，请重新输入");
+                i--;
+            } else {
+                for (int j = 0; j <(int) arr[i]; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+
 }
